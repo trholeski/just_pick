62bd2c087f2754dc7ba557b5d89875a8695ff3af
Tim Holeski <trholeski@gmail.com>
2019-07-14 16:21:30 -0700

    updated micromodal css to expand with padding around title as the contect required

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,18 +1,1 @@
-// import MicroModal from 'micromodal';  // es6 module
-// var MicroModal = require('micromodal'); // commonjs module
-
-// MicroModal.init({
-//     onShow: modal => console.info(`${modal.id} is shown`), // [1]
-//     onClose: modal => console.info(`${modal.id} is hidden`), // [2]
-//     openTrigger: 'data-custom-open', // [3]
-//     closeTrigger: 'data-custom-close', // [4]
-//     disableScroll: true, // [5]
-//     disableFocus: false, // [6]
-//     awaitCloseAnimation: false, // [7]
-//     debugMode: true // [8]
-//   });
-
 MicroModal.init();
-
-// MicroModal.show('modal-id'); // [1]
-// MicroModal.close('modal-id');
