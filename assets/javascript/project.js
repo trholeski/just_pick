dd1fe7129a6e32f0133d00e36f7d1bd7f21ee825
SilverBell17 <kevinjoun17@gmail.com>
2019-07-18 07:40:39 -0700

    added some comments

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,43 +1,39 @@
+//Declaring startLat and startLng as the lat/lng for Seattle. This will be default if we do not get geolocation.
 var startLat = 47.6062;
 var startLng = -122.3321;
+//results Array is declared to store the lat/lng for restaurant results.
 var resultsArray = [];
 
+//geolocation code. If geolocation is received, changes the startLat/Lng to the geolocation.
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         var pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
-        console.log(position);
         startLat = pos.lat
         startLng = pos.lng
-        console.log(startLat);
-
-    }, function () {
-    });
-} else {
-
+    },);
 }
-
+//function that creates the map. This will place at most 5 markers on the map based on the restaurant results. Map will center on geolocation.
 function initMap() {
-    console.log(startLat);
+    //creating variables for each of the markers.
     var geoLocation = { lat: startLat, lng: startLng };
     var marker1 = { lat: parseFloat(resultsArray[0]), lng: parseFloat(resultsArray[1]) };
     var marker2 = { lat: parseFloat(resultsArray[2]), lng: parseFloat(resultsArray[3]) };
     var marker3 = { lat: parseFloat(resultsArray[4]), lng: parseFloat(resultsArray[5]) };
     var marker4 = { lat: parseFloat(resultsArray[6]), lng: parseFloat(resultsArray[7]) };
     var marker5 = { lat: parseFloat(resultsArray[8]), lng: parseFloat(resultsArray[9]) };
-    // The map, centered at Uluru
+    //placing markers on the map.
     var map = new google.maps.Map(
         document.getElementById('mapResults'), { zoom: 11, center: geoLocation });
-    // The marker, positioned at Uluru
     var mapMarker1 = new google.maps.Marker({ position: marker1, map: map });
     var mapMarker2 = new google.maps.Marker({ position: marker2, map: map });
     var mapMarker3 = new google.maps.Marker({ position: marker3, map: map });
     var mapMarker4 = new google.maps.Marker({ position: marker4, map: map });
     var mapMarker5 = new google.maps.Marker({ position: marker5, map: map });
 }
-
+//creating variables for 
 var cuisines = [];
 var cuisineIDs = [];
 var lat = 47.6062;
@@ -47,27 +43,28 @@ var cuisineType = '';
 var cuisineID = 0;
 var restaurantList = [];
 
+//function that hides #resultsPage and shows #homepage
 function startPage() {
     $('#resultsPage').hide();
     $('#homepage').show();
-    console.log('working');
 }
-
+// on page load, run the function startPage.
 window.onload = startPage;
 
-
+//function that 
 function newPage(event) {
     event.preventDefault();
     $('#homepage').hide();
     restaurantQuery();
 };
 
+//function that shows results page, changes the html of zomresults.
 function restaurantQuery() {
     $('#resultsPage').show();
     $('#zomResults').html('');
     //create array of available cuisines by locations
     var queryURL = 'https://developers.zomato.com/api/v2.1/cuisines?city_id=' + cityID + '&lat=' + startLat + '&lon=' + startLng + '&count=5&apikey=77290d1b4dc1f21c65b6176dd07d56ed';
-    // code to push all cuisine types into the cuisines array.
+    // pushes all cuisine types into the cuisines array.
     $.ajax({
         url: queryURL,
         method: 'GET'
@@ -75,17 +72,19 @@ function restaurantQuery() {
         .then(function (response) {
             console.log(response);
             var results = response.cuisines;
+            //loops through the entire cuisines object and push to the array.
             for (var i = 0; i < results.length; i++) {
                 cuisines.push(results[i].cuisine.cuisine_name)
                 cuisineIDs.push(results[i].cuisine.cuisine_id)
             }
             console.log(cuisines)
-
+            //randomly generates a number from 0 to the number of cuisine types.
             var cuisineGenerator = Math.floor(Math.random() * cuisines.length) + 1;
-            cuisineType = cuisines[cuisineGenerator];
-            cuisineID = parseInt(cuisineIDs[cuisineGenerator]);
+            cuisineType = cuisines[cuisineGenerator]; //gets one cuisine (italian).
+            cuisineID = parseInt(cuisineIDs[cuisineGenerator]);//gets the cuisine ID of the cuisine.
             console.log(cuisineType);
             console.log(cuisineID);
+            //appends the random cuisine onto results page.
             $('#zomResults').append('<h3>' + "How about " + cuisineType + "?")
             $('#zomResults').append('<br>');
 
@@ -131,7 +130,7 @@ function restaurantQuery() {
         });
 };
 
-function searchQuery(event){
+function searchQuery() {
     event.preventDefault();
     var userSearch = $("#searchBar").val();
     console.log(userSearch);
