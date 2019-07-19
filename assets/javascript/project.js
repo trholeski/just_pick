4e9da71fdb3ccc18b0e1750d864e93a91ead0bd0
SilverBell17 <kevinjoun17@gmail.com>
2019-07-19 01:17:00 -0700

    combined working maps code with working modal code. Also did some cleaning up and commenting. Markers open the modal on click. There is a bug that it errors out unless you click the more info button first. Probably a scope issue with restaurantResults

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,11 +1,12 @@
-
+MicroModal.init();
 //Declaring startLat and startLng as the lat/lng for Seattle. This will be default if we do not get geolocation.
 var startLat = 47.6062;
 var startLng = -122.3321;
 //results Array is declared to store the lat/lng for restaurant results.
 var resultsArray = [];
+var restaurantResults = [];
 
-//geolocation code. If geolocation is received, changes the startLat/Lng to the geolocation.
+//geolocation code. If geolocation is received, changes the startLat/Lng to the geolocation. If no geolocation, uses default location seattle. Gets list of cuisines based off the area.
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         var pos = {
@@ -14,9 +15,8 @@ if (navigator.geolocation) {
         };
         startLat = pos.lat
         startLng = pos.lng
-        console.log(startLat, startLng);
         listCuisines();
-    });
+    }, listCuisines());
 }
 //function that creates the map. This will place at most 5 markers on the map based on the restaurant results. Map will center on geolocation.
 function initMap() {
@@ -35,20 +35,77 @@ function initMap() {
     var mapMarker3 = new google.maps.Marker({ position: marker3, map: map });
     var mapMarker4 = new google.maps.Marker({ position: marker4, map: map });
     var mapMarker5 = new google.maps.Marker({ position: marker5, map: map });
+    mapMarker1.addListener("click", function(){
+        var i = $(this).attr('restaurantIndex') //get the attribute "restaurantIndex" of the button clicked, assigns it to var i.
+        MicroModal.show('modal-2');//shows micromodal.
+        //adds a bunch of information to the micromodal. thumbnail, address, phone, cost, etc.
+        $('.card-img2').attr('src', restaurantResults[i].restaurant.thumb); //this code calling restaurantResults is actually calling the api for zomato. 
+        $('#restAddress').html("Address: " + restaurantResults[i].restaurant.location.address)
+        $('#restPhone').html("Phone: " + restaurantResults[i].restaurant.phone_numbers)
+        $('#restCost').html("Average Cost For Two: $" + restaurantResults[i].restaurant.average_cost_for_two)
+        $('#restMenu').html('<a target="_blank" href=' + restaurantResults[i].restaurant.menu_url + '>View the MENU</a>')
+        $('#restUrl').html('<a target="_blank" href=' + restaurantResults[i].restaurant.url + '>Restaurant Website</a>')
+        $('#modal-2-title').html(restaurantResults[i].restaurant.name)
+    });
+    mapMarker2.addListener("click", function(){
+        var i = $(this).attr('restaurantIndex') //get the attribute "restaurantIndex" of the button clicked, assigns it to var i.
+        MicroModal.show('modal-2');//shows micromodal.
+        //adds a bunch of information to the micromodal. thumbnail, address, phone, cost, etc.
+        $('.card-img2').attr('src', restaurantResults[i].restaurant.thumb); //this code calling restaurantResults is actually calling the api for zomato. 
+        $('#restAddress').html("Address: " + restaurantResults[i].restaurant.location.address)
+        $('#restPhone').html("Phone: " + restaurantResults[i].restaurant.phone_numbers)
+        $('#restCost').html("Average Cost For Two: $" + restaurantResults[i].restaurant.average_cost_for_two)
+        $('#restMenu').html('<a target="_blank" href=' + restaurantResults[i].restaurant.menu_url + '>View the MENU</a>')
+        $('#restUrl').html('<a target="_blank" href=' + restaurantResults[i].restaurant.url + '>Restaurant Website</a>')
+        $('#modal-2-title').html(restaurantResults[i].restaurant.name)
+    });
+    mapMarker3.addListener("click", function(){
+        var i = $(this).attr('restaurantIndex') //get the attribute "restaurantIndex" of the button clicked, assigns it to var i.
+        MicroModal.show('modal-2');//shows micromodal.
+        //adds a bunch of information to the micromodal. thumbnail, address, phone, cost, etc.
+        $('.card-img2').attr('src', restaurantResults[i].restaurant.thumb); //this code calling restaurantResults is actually calling the api for zomato. 
+        $('#restAddress').html("Address: " + restaurantResults[i].restaurant.location.address)
+        $('#restPhone').html("Phone: " + restaurantResults[i].restaurant.phone_numbers)
+        $('#restCost').html("Average Cost For Two: $" + restaurantResults[i].restaurant.average_cost_for_two)
+        $('#restMenu').html('<a target="_blank" href=' + restaurantResults[i].restaurant.menu_url + '>View the MENU</a>')
+        $('#restUrl').html('<a target="_blank" href=' + restaurantResults[i].restaurant.url + '>Restaurant Website</a>')
+        $('#modal-2-title').html(restaurantResults[i].restaurant.name)
+    });
+    mapMarker4.addListener("click", function(){
+        var i = $(this).attr('restaurantIndex') //get the attribute "restaurantIndex" of the button clicked, assigns it to var i.
+        MicroModal.show('modal-2');//shows micromodal.
+        //adds a bunch of information to the micromodal. thumbnail, address, phone, cost, etc.
+        $('.card-img2').attr('src', restaurantResults[i].restaurant.thumb); //this code calling restaurantResults is actually calling the api for zomato. 
+        $('#restAddress').html("Address: " + restaurantResults[i].restaurant.location.address)
+        $('#restPhone').html("Phone: " + restaurantResults[i].restaurant.phone_numbers)
+        $('#restCost').html("Average Cost For Two: $" + restaurantResults[i].restaurant.average_cost_for_two)
+        $('#restMenu').html('<a target="_blank" href=' + restaurantResults[i].restaurant.menu_url + '>View the MENU</a>')
+        $('#restUrl').html('<a target="_blank" href=' + restaurantResults[i].restaurant.url + '>Restaurant Website</a>')
+        $('#modal-2-title').html(restaurantResults[i].restaurant.name)
+    });
+    mapMarker5.addListener("click", function(){
+        var i = $(this).attr('restaurantIndex') //get the attribute "restaurantIndex" of the button clicked, assigns it to var i.
+        MicroModal.show('modal-2');//shows micromodal.
+        //adds a bunch of information to the micromodal. thumbnail, address, phone, cost, etc.
+        $('.card-img2').attr('src', restaurantResults[i].restaurant.thumb); //this code calling restaurantResults is actually calling the api for zomato. 
+        $('#restAddress').html("Address: " + restaurantResults[i].restaurant.location.address)
+        $('#restPhone').html("Phone: " + restaurantResults[i].restaurant.phone_numbers)
+        $('#restCost').html("Average Cost For Two: $" + restaurantResults[i].restaurant.average_cost_for_two)
+        $('#restMenu').html('<a target="_blank" href=' + restaurantResults[i].restaurant.menu_url + '>View the MENU</a>')
+        $('#restUrl').html('<a target="_blank" href=' + restaurantResults[i].restaurant.url + '>Restaurant Website</a>')
+        $('#modal-2-title').html(restaurantResults[i].restaurant.name)
+    });
 }
-
-MicroModal.init();
-
-
+//I removed variables lat, lon, and cityID.
+//variables that holds all cuisine types.
 var cuisines = [];
+//variable that holds all cuisine IDs.
 var cuisineIDs = [];
-var lat = 47.6062;
-var lon = 122.3321;
-var cityID = 279;
+//variable that holds the randomly selected cuisine type. Only for the "choose for me" button.
 var cuisineType = '';
+//variable that holds the randomly selected cuisine ID. Only for the "choose for me" button.
 var cuisineID = 0;
-var restaurantList = [];
-var restaurantResults = [];
+//variable for ?
 
 //function that hides #resultsPage and shows #homepage
 function startPage() {
@@ -58,8 +115,8 @@ function startPage() {
 // on page load, run the function startPage.
 window.onload = startPage;
 
-//function that 
-function newPage(event) {
+//function that hides the homepage and start restaurantQuery function
+function newPage() {
     event.preventDefault();
     $('#homepage').hide();
     restaurantQuery();
@@ -84,7 +141,7 @@ function listCuisines() {
         })
 }
 
-//function that shows results page, changes the html of zomresults.
+//shows resultsPage, changes the results fields to 5 restaurants of a randomly selected cuisine. Runs the map function to display the map with markers.
 function restaurantQuery() {
     $('#resultsPage').show();
     $('#zomResults').html('');
@@ -99,7 +156,6 @@ function restaurantQuery() {
     $('#zomResults').append('<h3>' + "How about " + cuisineType + "?")
     $('#zomResults').append('<br>');
 
-
     //pull restaurant info based on cuisineID
     var queryURL = 'https://developers.zomato.com/api/v2.1/search?lat=' + startLat + '&lon=' + startLng + '&cuisines=' + cuisineID + '&count=5&radius=8045&sort=rating&apikey=77290d1b4dc1f21c65b6176dd07d56ed';
 
@@ -109,13 +165,15 @@ function restaurantQuery() {
     })
         .then(function (response) {
             // console.log(response);
+            restaurantResults = response.restaurants;
             var results = response.restaurants;
+            console.log(results)
+            console.log(restaurantResults)
             // console.log(results[0].restaurant.name);
             //loops through the first 5 restaurant results and appends it onto the results page.
             for (var i = 0; i < 5; i++) {
                 // console.log(results[i].restaurant.location.latitude);
                 // console.log(results[i].restaurant.location.longitude);
-
 
                 var cardDiv1 = $('<div class=\'card mb-3\' style=\'max-width:540px\'>');
                 var cardDiv2 = $('<div class=\'row no-gutters\'>');
@@ -123,18 +181,14 @@ function restaurantQuery() {
                 var cardDiv3 = $('<div class=\'col-md-4\'>');
                 cardDiv2.append(cardDiv3);
                 var restaurantImg = $('<img class=\'restaurantImg card-img\'>');
-
                 restaurantImg.attr("src", results[i].restaurant.thumb);
-
                 cardDiv3.append(restaurantImg);
                 var cardDiv4 = $('<div class=\'col-md-8\'>');
                 cardDiv2.append(cardDiv4);
                 var cardDiv5 = $('<div class=\'card-body\'>');
-
                 cardDiv5.append('<h5 class=\'card-title\'>' + results[i].restaurant.name + '</h5>');
                 cardDiv5.append('<p class=\'card-text\'>' + 'Restaurant Rating: ' + results[i].restaurant.user_rating.aggregate_rating + '/5' + '</p>');
-                cardDiv5.append('<p class=\'card-text\'>' + results[i].restaurant.location.address + '</p>');
-                cardDiv5.append('<button type=\'button\' class=\'btn btn-light modalBtn\'>' + 'More info' + '</button>');
+                cardDiv5.append("<button type='button' class='btn btn-secondary' id='buttonClicker'   restaurantIndex='" + i + "'>  More info  </button>");
                 cardDiv4.append(cardDiv5);
                 $('#zomResults').append(cardDiv1);
                 if (i === 0) {
@@ -144,14 +198,14 @@ function restaurantQuery() {
                 } else {
                     resultsArray.push(results[i].restaurant.location.latitude);
                     resultsArray.push(results[i].restaurant.location.longitude);
-                    // console.log(resultsArray);
                 }
+                restaurantResults = response.restaurants;
                 initMap();
             }
         });
 };
 
-//function that gets the value of user input. Also checks if this is a cuisine. IF it is, run function that calls api based off cuisine id.
+//function that runs when the search button is clicked from the home screen. Takes user input, searches for restaurants based off location, displays the results in the results field and the map with markers. 
 function searchQueryHome() {
     event.preventDefault();
     $('#homepage').hide();
@@ -169,9 +223,8 @@ function searchQueryHome() {
         method: 'GET'
     })
         .then(function (response) {
-            // console.log(response);
+            restaurantResults = response.restaurants;
             var results = response.restaurants;
-            // console.log(results[0].restaurant.name);
             //loops through the first 5 restaurant results and appends it onto the results page.
             for (var i = 0; i < 5; i++) {
                 // console.log(results[i].restaurant.location.latitude);
@@ -190,8 +243,7 @@ function searchQueryHome() {
                 var cardDiv5 = $('<div class=\'card-body\'>');
                 cardDiv5.append('<h5 class=\'card-title\'>' + results[i].restaurant.name + '</h5>');
                 cardDiv5.append('<p class=\'card-text\'>' + 'Restaurant Rating: ' + results[i].restaurant.user_rating.aggregate_rating + '/5' + '</p>');
-                cardDiv5.append('<p class=\'card-text\'>' + results[i].restaurant.location.address + '</p>');
-                cardDiv5.append('<button type=\'button\' class=\'btn btn-light modalBtn\'>' + 'More info' + '</button>');
+                cardDiv5.append("<button type='button' class='btn btn-secondary' id='buttonClicker'   restaurantIndex='" + i + "'>  More info  </button>");
                 cardDiv4.append(cardDiv5);
                 $('#zomResults').append(cardDiv1);
                 if (i === 0) {
@@ -201,13 +253,12 @@ function searchQueryHome() {
                 } else {
                     resultsArray.push(results[i].restaurant.location.latitude);
                     resultsArray.push(results[i].restaurant.location.longitude);
-                    // console.log(resultsArray);
                 }
                 initMap();
             }
         });
 };
-
+//function that runs when the search button is clicked from the results screen. Takes user input, searches for restaurants based off location, displays the results in the results field and the map with markers. 
 function searchQueryResults() {
     event.preventDefault();
     $('#homepage').hide();
@@ -225,9 +276,8 @@ function searchQueryResults() {
         method: 'GET'
     })
         .then(function (response) {
-            // console.log(response);
+            restaurantResults = response.restaurants;
             var results = response.restaurants;
-            // console.log(results[0].restaurant.name);
             //loops through the first 5 restaurant results and appends it onto the results page.
             for (var i = 0; i < 5; i++) {
                 // console.log(results[i].restaurant.location.latitude);
@@ -246,8 +296,7 @@ function searchQueryResults() {
                 var cardDiv5 = $('<div class=\'card-body\'>');
                 cardDiv5.append('<h5 class=\'card-title\'>' + results[i].restaurant.name + '</h5>');
                 cardDiv5.append('<p class=\'card-text\'>' + 'Restaurant Rating: ' + results[i].restaurant.user_rating.aggregate_rating + '/5' + '</p>');
-                cardDiv5.append('<p class=\'card-text\'>' + results[i].restaurant.location.address + '</p>');
-                cardDiv5.append('<button type=\'button\' class=\'btn btn-light modalBtn\'>' + 'More info' + '</button>');
+                cardDiv5.append("<button type='button' class='btn btn-secondary' id='buttonClicker'   restaurantIndex='" + i + "'>  More info  </button>");
                 cardDiv4.append(cardDiv5);
                 $('#zomResults').append(cardDiv1);
                 if (i === 0) {
@@ -257,33 +306,52 @@ function searchQueryResults() {
                 } else {
                     resultsArray.push(results[i].restaurant.location.latitude);
                     resultsArray.push(results[i].restaurant.location.longitude);
-                    // console.log(resultsArray);
                 }
                 initMap();
             }
         });
-
 };
-// What happens when you click on "get info" on the restaurant result page.  This is how the modals are built
-$('#zomResults').on('click', '#buttonClicker', function(){
-    var i = $(this).attr('restaurantIndex')
-    console.log('restaurant results=', restaurantResults);
-    console.log('the is i:', i);
-    MicroModal.show('modal-2');
-    $('#modal-2-title').html(restaurantResults[i].restaurant.name);
-    $('#modal-2-content').html("Address:  " + restaurantResults[i].restaurant.location.address);
+
+//Micromodal function:
+//when a "more info" button is clicked
+$('#zomResults').on('click', '#buttonClicker', function () {//run this function.
+    var i = $(this).attr('restaurantIndex') //get the attribute "restaurantIndex" of the button clicked, assigns it to var i.
+    MicroModal.show('modal-2');//shows micromodal.
+    //adds a bunch of information to the micromodal. thumbnail, address, phone, cost, etc.
+    $('.card-img2').attr('src', restaurantResults[i].restaurant.thumb); //this code calling restaurantResults is actually calling the api for zomato. 
+    $('#restAddress').html("Address: " + restaurantResults[i].restaurant.location.address)
+    $('#restPhone').html("Phone: " + restaurantResults[i].restaurant.phone_numbers)
+    $('#restCost').html("Average Cost For Two: $" + restaurantResults[i].restaurant.average_cost_for_two)
+    $('#restMenu').html('<a target="_blank" href=' + restaurantResults[i].restaurant.menu_url + '>View the MENU</a>')
+    $('#restUrl').html('<a target="_blank" href=' + restaurantResults[i].restaurant.url + '>Restaurant Website</a>')
+    $('#modal-2-title').html(restaurantResults[i].restaurant.name)
 });
 
+// $('#mapResults').on('click', '', function () {//run this function.
+//     var i = $(this).attr('restaurantIndex') //get the attribute "restaurantIndex" of the button clicked, assigns it to var i.
+//     MicroModal.show('modal-2');//shows micromodal.
+//     //adds a bunch of information to the micromodal. thumbnail, address, phone, cost, etc.
+//     $('.card-img2').attr('src', restaurantResults[i].restaurant.thumb); //this code calling restaurantResults is actually calling the api for zomato. 
+//     $('#restAddress').html("Address: " + restaurantResults[i].restaurant.location.address)
+//     $('#restPhone').html("Phone: " + restaurantResults[i].restaurant.phone_numbers)
+//     $('#restCost').html("Average Cost For Two: $" + restaurantResults[i].restaurant.average_cost_for_two)
+//     $('#restMenu').html('<a target="_blank" href=' + restaurantResults[i].restaurant.menu_url + '>View the MENU</a>')
+//     $('#restUrl').html('<a target="_blank" href=' + restaurantResults[i].restaurant.url + '>Restaurant Website</a>')
+//     $('#modal-2-title').html(restaurantResults[i].restaurant.name)
+// });
 
 
 
-// console.log(userSearch);
 
+//when "Choose for me" is clicked from the results screen, run restaurantQuery()
 $('.chooseBtnResults').on('click', restaurantQuery);
+//when the "choose for me" on home screen is clicked, run newPage().
 $('#chooseBtnID').on('click', newPage);
-
+//when search button is clicked from home screen, run function.
 $("#userSearchHome").on("click", searchQueryHome);
+//when search button is clicked from the results screen, run function.
 $("#userSearchResults").on("click", searchQueryResults);
+
 
 
 //user validation that checks the user input to the cuisine types.
